13f24ceb1fc8a0f072ec1689a9ef69f80431dd37
Sigitas <sig.kublickas99@gmail.com>
2024-05-23 00:29:23 +0300

    add repositories

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -2,94 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Category;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
-use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function getGroupedItems($arr, $name)
-    {
-        $group = $arr->groupBy($name);
-        return $group->map(function ($products, $brand) {
-            return [
-                'name' => $brand,
-                'count' => $products->count(),
-            ];
-        })->values();
+    protected $productRepository;
 
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
     }
 
-    public function index($category_path, Request $request)
+    public function index($path, Request $request)
     {
-        $category = Category::where('path', $category_path)->first();
-        $productsQuery = Product::where("category_id", $category->id);
-
-        $parameters = $request->all();
-        foreach ($parameters as $key => $param) {
-            $paramArr = explode("%", $param);
-            $productsQuery->whereIn($key, $paramArr);
-        }
-        $filters = ["brand", "model", "delivery_time", "condition", "price"];
-
-        $products = $productsQuery->get();
-
-        $group = [];
-        foreach ($filters as $item) {
-            if (array_key_first($parameters) == $item) {
-                $pro = Product::where("category_id", $category->id)->get();
-                array_push($group, $this->getGroupedItems($pro, $item));
-            } else {
-                array_push($group, $this->getGroupedItems($products, $item));
-            }
-        }
-
-
-
-
-        return response()->json(
-            [
-                [
-                    "products" => $products,
-                    "filters" => [
-                        [
-                            "name" => "Gamintojas",
-                            'table_name' => "brand",
-                            "items" => $group[0]
-                        ],
-                        [
-                            "name" => "Modelis",
-                            'table_name' => "model",
-                            "items" => $group[1],
-                        ],
-                        [
-                            "name" => "Pristatymo laikas",
-                            'table_name' => "delivery_time",
-                            "items" => $group[2],
-                        ],
-                        [
-                            "name" => "Būklė",
-                            'table_name' => "condition",
-                            "items" => $group[3],
-                        ],
-                        [
-                            "name" => "Kaina",
-                            'table_name' => "price",
-                            "items" => $group[4],
-                        ]
-                    ]
-                ],
-
-
-            ]
-        );
-
+        return $this->productRepository->getAllFiltersAndProducts($path, $request);
     }
 
     /**
@@ -106,35 +36,7 @@ class ProductController extends Controller
 
     public function storeXml(Request $request)
     {
-        if ($request->header('Content-Type') === 'application/xml') {
-            $xmlData = $request->getContent();
-
-            try {
-                $data = simplexml_load_string($xmlData, "SimpleXMLElement");
-
-                $json = json_encode($data);
-                $array = json_decode($json, true);
-                print_r($array);
-
-                $products = isset($array['product'][0]) ? $array['product'] : [$array['product']];
-                $requestClassInstance = new StoreProductRequest();
-                $rules = $requestClassInstance->rules();
-                foreach ($products as $item) {
-                    $validator = Validator::make($item, $rules);
-                    if ($validator->fails()) {
-                        return response()->json(['message' => $validator->errors()], 400);
-                    }
-
-                    Product::create($validator->validated());
-                }
-
-                return response()->json(['message' => 'Sėkmingai sukurta'], 200);
-            } catch (\Exception $e) {
-                return response()->json(['message' => $e->getMessage()], 400);
-            }
-        } else {
-            return response()->json(['message' => 'Klaida! Xml formatas netinkamas'], 400);
-        }
+        return $this->productRepository->createProductXml($request);
     }
     public function store(StoreProductRequest $request)
     {
@@ -197,14 +99,5 @@ class ProductController extends Controller
     {
         //
     }
-    //get all products by subcategory_id
-    public function showAllByPath($category_id)
-    {
-        $products = Product::where('category_id', $category_id)->get();
-        if ($products->isEmpty()) {
-            return ['message' => 'Produktu nerasta pagal šią kategoriją !'];
-        }
-        return $products;
 
-    }
 }
--- backend/app/Repositories/ProductRepository.php
+++ backend/app/Repositories/ProductRepository.php
@@ -1,8 +1,109 @@
 <?php
 
-namespace app\Repositories;
+namespace App\Repositories;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StoreProductRequest;
 
 class ProductRepository
 {
+    public function getGroupedItems($arr, $name)
+    {
+        return $arr->groupBy($name)->map(function ($products, $brand) {
+            return [
+                'name' => $brand,
+                'count' => $products->count(),
+            ];
+        })->values();
+    }
 
+    public function namingFilters($filter)
+    {
+        switch ($filter) {
+            case "brand":
+                return "Prekės ženklas";
+            case "model":
+                return "Modelis";
+            case "delivery_time":
+                return "Pristatymo laikas (dienos)";
+            case "condition":
+                return "Būklė";
+            case "price":
+                return "Kaina";
+            default:
+                return "";
+        }
+    }
+    public function getAllFiltersAndProducts($path, Request $request)
+    {
+        $category = Category::where('path', $path)->firstOrFail();
+        $productsQuery = Product::where('category_id', $category->id);
+
+        $parameters = $request->all();
+
+        foreach ($parameters as $key => $param) {
+            $paramArr = explode('%', $param);
+            $productsQuery->whereIn($key, $paramArr);
+        }
+
+        $filters = ['brand', 'model', 'delivery_time', 'condition', 'price'];
+
+        $products = $productsQuery->get();
+
+        $groupedFilters = collect($filters)->map(function ($filter) use ($parameters, $category, $products) {
+            if (array_key_first($parameters) === $filter) {
+                $allProducts = Product::where('category_id', $category->id)->get();
+                return $this->getGroupedItems($allProducts, $filter);
+            } else {
+                return $this->getGroupedItems($products, $filter);
+            }
+        });
+
+        return response()->json([
+            'products' => $products,
+            'filters' => $groupedFilters->map(function ($items, $index) use ($filters) {
+                return [
+                    'name' => $this->namingFilters($filters[$index]),
+                    'table_name' => $filters[$index],
+                    'items' => $items
+                ];
+            }),
+        ]);
+    }
+
+    public function createProductXml(Request $request)
+    {
+        if ($request->header('Content-Type') === 'application/xml') {
+            $xmlData = $request->getContent();
+
+            try {
+                $data = simplexml_load_string($xmlData, "SimpleXMLElement");
+
+                $json = json_encode($data);
+                $array = json_decode($json, true);
+                print_r($array);
+
+                $products = isset($array['product'][0]) ? $array['product'] : [$array['product']];
+                $requestClassInstance = new StoreProductRequest();
+                $rules = $requestClassInstance->rules();
+                foreach ($products as $item) {
+                    $validator = Validator::make($item, $rules);
+                    if ($validator->fails()) {
+                        return response()->json(['message' => $validator->errors()], 400);
+                    }
+
+                    Product::create($validator->validated());
+                }
+
+                return response()->json(['message' => 'Sėkmingai sukurta'], 200);
+            } catch (\Exception $e) {
+                return response()->json(['message' => $e->getMessage()], 400);
+            }
+        } else {
+            return response()->json(['message' => 'Klaida! Xml formatas netinkamas'], 400);
+        }
+    }
 }
